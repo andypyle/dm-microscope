3ba3f3423d33508dcd951678cc4f1034c7963b5a
Andrew Pyle <andy.pyle@gmail.com>
2015-05-24 01:44:32 -0700

    Added user profile image from Google.

--- server/publications.js
+++ server/publications.js
@@ -6,3 +6,7 @@ Meteor.publish('comments', function(postId){
 	check(postId, String);
 	return Comments.find({postId: postId});
 });
+
+Meteor.publish('userImages', function(){
+	return Meteor.users.find({},{fields:{"services.google.picture":true}});
+});
